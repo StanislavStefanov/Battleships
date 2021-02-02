32004233b5a110295c765f3d6985c9aa86c87b84
Stanislav Stefanov <stanislavsvetlinovstefanov@gmail.com>
2021-02-03 00:17:34 +0200

    add room implementation and tests

--- server/room.go
+++ server/room.go
@@ -1,1 +1,343 @@
 package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/StanislavStefanov/Battleships/server/board"
+	"github.com/StanislavStefanov/Battleships/server/player"
+	"github.com/StanislavStefanov/Battleships/utils"
+	"github.com/gorilla/websocket"
+	"log"
+	"sync"
+)
+
+type Room struct {
+	Current         *player.Player
+	Next            *player.Player
+	Done            chan struct{}
+	Phase           string
+	ShipSizeToCount map[int]int
+	NextShipSize    int
+	Id              string
+	ResponseSender  ResponseSender
+}
+
+const (
+	destroyer  = 5
+	battleship = 4
+	ship       = 3
+	boat       = 2
+)
+
+const (
+	destroyerCount  = 2
+	battleshipCount = 4
+	shipCount       = 6
+	boatCount       = 8
+)
+
+//TODO new fields
+func CreateRoom(id string, player *player.Player, done chan struct{}) Room {
+	return Room{
+		Current:         player,
+		Next:            nil,
+		Done:            done,
+		Phase:           "wait",
+		ShipSizeToCount: map[int]int{destroyer: destroyerCount, battleship: battleshipCount, ship: shipCount, boat: boatCount},
+		NextShipSize:    destroyer,
+		Id:              id,
+	}
+}
+
+func (r *Room) Join(player *player.Player) error {
+	if r.Next != nil {
+		return errors.New("room is already full")
+	}
+	r.Next = player
+	return nil
+}
+
+func (r *Room) GetRoomInfo() (string, int) {
+	playersCount := 0
+	if r.Current != nil {
+		playersCount++
+	}
+	if r.Next != nil {
+		playersCount++
+	}
+	return r.Id, playersCount
+}
+
+const (
+	Exit      = "exit"
+	Shoot     = "shoot"
+	PlaceShip = "place"
+	Wait      = "wait"
+	Retry     = "retry"
+	Win       = "win"
+	Lose      = "lose"
+)
+
+func (r *Room) ProcessCommand(request utils.Request) {
+	id := request.GetId()
+	if id != r.Current.Id {
+		resp := utils.BuildResponse(Wait, "Wait for enemy to make his turn.", nil)
+		r.ResponseSender.SendResponse(resp, r.Next.Conn)
+		return
+	}
+
+	action := request.GetAction()
+	if action != r.Phase && action != Exit {
+		resp := utils.BuildResponse(Retry,
+			fmt.Sprintf("Invalid action during Phase: %s.", r.Phase),
+			nil)
+		r.ResponseSender.SendResponse(resp, r.Current.Conn)
+		return
+	}
+
+	switch action {
+	case PlaceShip:
+		r.processShipPlacement(request)
+	case Shoot:
+		r.processShoot(request)
+	case Exit:
+		resp := utils.BuildResponse(Win, "Your opponent exited the game. Congratulations, you win!", nil)
+		r.ResponseSender.SendResponse(resp, r.Next.Conn)
+		r.Done <- struct{}{}
+	}
+}
+
+func (r *Room) processShipPlacement(request utils.Request) {
+	err := r.placeShip(request)
+	if err != nil {
+		resp := utils.BuildResponse(Retry, err.Error(), nil)
+		r.ResponseSender.SendResponse(resp, r.Current.Conn)
+		return
+	}
+
+	resp := utils.BuildResponse(Wait,
+		"Ship placed successfully. Wait for opponent to make his turn.",
+		nil)
+	r.ResponseSender.SendResponse(resp, r.Current.Conn)
+
+	length, err := r.getNextShipSize()
+	if err != nil {
+		r.Phase = Shoot
+		response := utils.BuildResponse(Shoot, "Select filed to attack.", nil)
+		r.ResponseSender.SendResponse(response, r.Next.Conn)
+		r.switchPlayers()
+		return
+	}
+
+	resp = utils.BuildResponse(PlaceShip,
+		fmt.Sprintf("Select where to place ship with length %d", length),
+		nil)
+	r.ResponseSender.SendResponse(resp, r.Next.Conn)
+
+	r.switchPlayers()
+}
+
+func (r *Room) placeShip(req utils.Request) error {
+	ship, err := getShip(req)
+	if err != nil {
+		return err
+	}
+
+	ship.SetLength(r.NextShipSize)
+
+	return r.Current.PlaceShip(*ship)
+}
+
+func getShip(req utils.Request) (*board.Ship, error) {
+	args := req.GetArgs()
+
+	x, err := extractIntFromArgs("x", args)
+	if err != nil {
+		return nil, err
+	}
+
+	y, err := extractIntFromArgs("y", args)
+	if err != nil {
+		return nil, err
+	}
+
+	direction, err := extractStringFromArgs("direction", args)
+	if err != nil {
+		return nil, err
+	}
+
+	length, err := extractIntFromArgs("length", args)
+	if err != nil {
+		return nil, err
+	}
+	ship := board.CreateShip(x, y, direction, length)
+	return &ship, err
+}
+
+func (r *Room) getNextShipSize() (int, error) {
+	count := r.ShipSizeToCount[r.NextShipSize]
+	if count > 0 {
+		r.ShipSizeToCount[r.NextShipSize] = count - 1
+		return r.NextShipSize, nil
+	}
+
+	r.NextShipSize--
+	if r.NextShipSize < 2 {
+		return 0, errors.New("all ships already placed")
+	}
+	count = r.ShipSizeToCount[r.NextShipSize]
+	r.ShipSizeToCount[r.NextShipSize] = count - 1
+
+	return r.NextShipSize, nil
+}
+
+func (r *Room) processShoot(request utils.Request) {
+	position, err := getPosition(request)
+	if err != nil {
+		resp := utils.BuildResponse(Retry, err.Error(), nil)
+		r.ResponseSender.SendResponse(resp, r.Current.Conn)
+		return
+	}
+
+	success, err := r.shootAtField(*position)
+	if err != nil {
+		resp := utils.BuildResponse(Retry, err.Error(), nil)
+		r.ResponseSender.SendResponse(resp, r.Current.Conn)
+		return
+	}
+
+	if success && r.Next.Board.IsBeaten() {
+		resp := utils.BuildResponse(Win, "Congratulations, you win!", nil)
+		r.ResponseSender.SendResponse(resp, r.Current.Conn)
+
+		resp = utils.BuildResponse(Lose, "Defeat!", nil)
+		r.ResponseSender.SendResponse(resp, r.Next.Conn)
+
+		r.Done <- struct{}{}
+		return
+	}
+
+	args := make(map[string]interface{})
+	args["hit"] = success
+	args["x"] = position.X
+	args["y"] = position.Y
+
+	resp := utils.BuildResponse(Wait, "", args)
+	r.ResponseSender.SendResponse(resp, r.Current.Conn)
+
+	resp = utils.BuildResponse(Shoot, "Select filed to attack.", args)
+	r.ResponseSender.SendResponse(resp, r.Next.Conn)
+
+	r.switchPlayers()
+
+}
+
+func (r *Room) shootAtField(position board.Position) (bool, error) {
+	success, err := r.Next.Board.ReceiveAttack(position)
+	if err != nil {
+		return false, err
+	}
+
+	r.Current.Board.Attack(position, success)
+	return true, nil
+}
+
+func (r *Room) switchPlayers() {
+	p := r.Next
+	r.Next = r.Current
+	r.Current = p
+}
+
+func (r *Room) closeRoom() {
+	_ = r.Current.Conn.Close()
+	_ = r.Next.Conn.Close()
+}
+
+func getPosition(req utils.Request) (*board.Position, error) {
+	args := req.GetArgs()
+
+	x, err := extractIntFromArgs("x", args)
+	if err != nil {
+		return nil, err
+	}
+
+	y, err := extractIntFromArgs("y", args)
+	if err != nil {
+		return nil, err
+	}
+
+	return &board.Position{
+		X: x,
+		Y: y,
+	}, nil
+}
+
+func extractIntFromArgs(key string, args map[string]interface{}) (int, error) {
+	v, ok := args[key]
+	if !ok {
+		return 0, errors.New(fmt.Sprintf("missing value for %s", key))
+	}
+	value, ok := v.(int)
+	if !ok {
+		return 0, errors.New(fmt.Sprintf("invalid value for %s", key))
+	}
+
+	return value, nil
+}
+
+func extractStringFromArgs(key string, args map[string]interface{}) (string, error) {
+	v, ok := args[key]
+	if !ok {
+		return "", errors.New(fmt.Sprintf("missing value for %s", key))
+	}
+	value, ok := v.(string)
+	if !ok {
+		return "", errors.New(fmt.Sprintf("invalid value for %s", key))
+	}
+
+	return value, nil
+}
+
+func (s *Server) runRoom(r *Room, done <-chan struct{}) {
+	var wg *sync.WaitGroup
+
+	first := make(chan utils.Request)
+	wg.Add(1)
+	go playerReadLoop(r.Current.Conn, first, wg)
+
+	second := make(chan utils.Request)
+	wg.Add(1)
+	go playerReadLoop(r.Next.Conn, second, wg)
+
+	for {
+		select {
+		case request := <-first:
+			r.ProcessCommand(request)
+		case request := <-second:
+			r.ProcessCommand(request)
+		case <-done:
+			r.closeRoom()
+			s.deleteRoom(r.Id)
+			wg.Wait()
+			return
+		}
+	}
+}
+
+func playerReadLoop(conn *websocket.Conn, play chan utils.Request, wg *sync.WaitGroup) {
+	//	cancel, cancelFunc := context.WithCancel(context.Background())
+	fmt.Println("start Current read loop")
+	defer wg.Done()
+	for {
+		_, bytes, err := conn.ReadMessage()
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		var req utils.Request
+		json.Unmarshal(bytes, &req)
+		play <- req
+	}
+}
--- server/room_test.go
+++ server/room_test.go
@@ -1,1 +1,680 @@
 package main
+
+import (
+	"github.com/StanislavStefanov/Battleships/server/automock"
+	"github.com/StanislavStefanov/Battleships/server/board"
+	"github.com/StanislavStefanov/Battleships/server/player"
+	"github.com/StanislavStefanov/Battleships/utils"
+	"github.com/gorilla/websocket"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRoom_GetNextShipSize(t *testing.T) {
+	t.Run("get all ship sizes", func(t *testing.T) {
+		// when
+		room := CreateRoom("", nil, nil)
+		expectedSizes := []int{5, 5, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2}
+		// then
+		for i := 0; i < 20; i++ {
+			size, err := room.getNextShipSize()
+			assert.NoError(t, err)
+			assert.Equal(t, expectedSizes[i], size)
+		}
+	})
+
+	t.Run("fail, all ships already placed", func(t *testing.T) {
+		// when
+		room := CreateRoom("", nil, nil)
+		room.ShipSizeToCount = map[int]int{2: 0}
+		room.NextShipSize = 2
+		// then
+		_, err := room.getNextShipSize()
+		assert.Equal(t, "all ships already placed", err.Error())
+	})
+}
+
+func TestRoom_Join(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		// when
+		first := player.Player{}
+		room := CreateRoom("", &first, nil)
+
+		// then
+		second := player.Player{}
+		err := room.Join(&second)
+		assert.NoError(t, err)
+		assert.Equal(t, &second, room.Next)
+	})
+
+	t.Run("fail", func(t *testing.T) {
+		// when
+		first := player.Player{}
+		room := CreateRoom("", &first, nil)
+		second := player.Player{}
+		err := room.Join(&second)
+		assert.NoError(t, err)
+		// then
+		third := player.Player{}
+		err = room.Join(&third)
+		assert.Equal(t, "room is already full", err.Error())
+	})
+}
+
+func TestRoom_GetRoomInfo(t *testing.T) {
+	t.Run("One place taken", func(t *testing.T) {
+		// when
+		first := player.Player{}
+		room := CreateRoom("", &first, nil)
+
+		// then
+		_, count := room.GetRoomInfo()
+		assert.Equal(t, 1, count)
+	})
+
+	t.Run("Room is full", func(t *testing.T) {
+		// when
+		first := player.Player{}
+		room := CreateRoom("", &first, nil)
+		second := player.Player{}
+		err := room.Join(&second)
+		assert.NoError(t, err)
+		// then
+		_, count := room.GetRoomInfo()
+		assert.Equal(t, 2, count)
+	})
+
+	t.Run("One place taken", func(t *testing.T) {
+		// when
+		first := player.Player{}
+		room := CreateRoom("room-id", &first, nil)
+
+		// then
+		id, _ := room.GetRoomInfo()
+		assert.Equal(t, "room-id", id)
+	})
+}
+
+var firstConn = &websocket.Conn{}
+var secondConn = &websocket.Conn{}
+
+func getBoard() *board.Board {
+	b := board.InitBoard()
+	ship := board.CreateShip(3, 3, "down", 4)
+	b.PlaceShip(ship)
+	return b
+}
+
+func getBoardWithOneTakenField(x, y int) *board.Board {
+	b := board.InitBoard()
+	ship := board.CreateShip(x, y, "down", 1)
+	b.PlaceShip(ship)
+	return b
+}
+
+func TestShip_ProcessCommand(t *testing.T) {
+	var (
+		waitResp = utils.Response{
+			Action:  Wait,
+			Message: "Wait for enemy to make his turn.",
+			Args:    nil,
+		}
+
+		invalidActionResp = utils.Response{
+			Action:  Retry,
+			Message: "Invalid action during Phase: place.",
+			Args:    nil,
+		}
+
+		missingXResp = utils.Response{
+			Action:  Retry,
+			Message: "missing value for x",
+			Args:    nil,
+		}
+
+		incorrectValueTypeResp = utils.Response{
+			Action:  Retry,
+			Message: "invalid value for y",
+			Args:    nil,
+		}
+
+		missingValueForDirectionResp = utils.Response{
+			Action:  Retry,
+			Message: "missing value for direction",
+			Args:    nil,
+		}
+
+		incorrectValueTypeForDirectionResp = utils.Response{
+			Action:  Retry,
+			Message: "invalid value for direction",
+			Args:    nil,
+		}
+
+		missingValueForLengthResp = utils.Response{
+			Action:  Retry,
+			Message: "missing value for length",
+			Args:    nil,
+		}
+
+		incorrectValueTypeForLengthResp = utils.Response{
+			Action:  Retry,
+			Message: "invalid value for length",
+			Args:    nil,
+		}
+
+		shipPlacedSuccessfullyResp = utils.Response{
+			Action:  Wait,
+			Message: "Ship placed successfully. Wait for opponent to make his turn.",
+			Args:    nil,
+		}
+
+		placeShipResp = utils.Response{
+			Action:  PlaceShip,
+			Message: "Select where to place ship with length 5",
+			Args:    nil,
+		}
+
+		shootResp = utils.Response{
+			Action:  Shoot,
+			Message: "Select filed to attack.",
+			Args:    nil,
+		}
+
+		shootOutOfBoundsResp = utils.Response{
+			Action:  Retry,
+			Message: "position out of bounds",
+			Args:    nil,
+		}
+
+		shootHitResp = utils.Response{
+			Action:  Wait,
+			Message: "",
+			Args:    map[string]interface{}{"hit": true, "x": 3, "y": 3},
+		}
+
+		shootWithArgsResp = utils.Response{
+			Action:  Shoot,
+			Message: "Select filed to attack.",
+			Args:    map[string]interface{}{"hit": true, "x": 3, "y": 3},
+		}
+
+		winResp = utils.Response{
+			Action:  Win,
+			Message: "Congratulations, you win!",
+			Args:    nil,
+		}
+
+		defeatResp = utils.Response{
+			Action:  Lose,
+			Message: "Defeat!",
+			Args:    nil,
+		}
+		exitResp = utils.Response{
+			Action:  Win,
+			Message: "Your opponent exited the game. Congratulations, you win!",
+			Args:    nil,
+		}
+	)
+	firstID := "first"
+	secondID := "second"
+	// given
+	testCases := []struct {
+		Name           string
+		Room           *Room
+		Request        utils.Request
+		Phase          string
+		CurrentID      string
+		NextID         string
+		ResponseSender func() *automock.ResponseSender
+	}{
+		{
+			Name: "fail when it is not your turn",
+			Room: &Room{
+				Current: &player.Player{
+					Id: firstID,
+				},
+				Next: &player.Player{
+					Id:   secondID,
+					Conn: secondConn,
+				},
+				Phase: PlaceShip,
+			},
+			Request: utils.Request{
+				PlayerId: secondID,
+			},
+			Phase:     PlaceShip,
+			CurrentID: firstID,
+			NextID:    secondID,
+			ResponseSender: func() *automock.ResponseSender {
+				sender := &automock.ResponseSender{}
+				sender.On("SendResponse", waitResp, secondConn).Return(nil).Once()
+				return sender
+			},
+		},
+		{
+			Name: "fail when invalid action during phase",
+			Room: &Room{
+				Current: &player.Player{
+					Id:   firstID,
+					Conn: firstConn,
+				},
+				Next: &player.Player{
+					Id: secondID,
+				},
+				Phase: PlaceShip,
+			},
+			Request: utils.Request{
+				PlayerId: firstID,
+				Action:   Shoot,
+			},
+			Phase:     PlaceShip,
+			CurrentID: firstID,
+			NextID:    secondID,
+			ResponseSender: func() *automock.ResponseSender {
+				sender := &automock.ResponseSender{}
+				sender.On("SendResponse", invalidActionResp, firstConn).Return(nil).Once()
+				return sender
+			},
+		},
+		{
+			Name: "fail when x coordinate for ship is missing",
+			Room: &Room{
+				Current: &player.Player{
+					Id:   firstID,
+					Conn: firstConn,
+				},
+				Next: &player.Player{
+					Id: secondID,
+				},
+				Phase: PlaceShip,
+			},
+			Request: utils.Request{
+				PlayerId: firstID,
+				Action:   PlaceShip,
+			},
+			Phase:     PlaceShip,
+			CurrentID: firstID,
+			NextID:    secondID,
+			ResponseSender: func() *automock.ResponseSender {
+				sender := &automock.ResponseSender{}
+				sender.On("SendResponse", missingXResp, firstConn).Return(nil).Once()
+				return sender
+			},
+		},
+		{
+			Name: "fail when y coordinate for ship has wrong type",
+			Room: &Room{
+				Current: &player.Player{
+					Id:   firstID,
+					Conn: firstConn,
+				},
+				Next: &player.Player{
+					Id: secondID,
+				},
+				Phase: PlaceShip,
+			},
+			Request: utils.Request{
+				PlayerId: firstID,
+				Action:   PlaceShip,
+				Args:     map[string]interface{}{"x": 2, "y": "y"},
+			},
+			Phase:     PlaceShip,
+			CurrentID: firstID,
+			NextID:    secondID,
+			ResponseSender: func() *automock.ResponseSender {
+				sender := &automock.ResponseSender{}
+				sender.On("SendResponse", incorrectValueTypeResp, firstConn).Return(nil).Once()
+				return sender
+			},
+		},
+		{
+			Name: "fail when direction is missing",
+			Room: &Room{
+				Current: &player.Player{
+					Id:   firstID,
+					Conn: firstConn,
+				},
+				Next: &player.Player{
+					Id: secondID,
+				},
+				Phase: PlaceShip,
+			},
+			Request: utils.Request{
+				PlayerId: firstID,
+				Action:   PlaceShip,
+				Args:     map[string]interface{}{"x": 2, "y": 2},
+			},
+			Phase:     PlaceShip,
+			CurrentID: firstID,
+			NextID:    secondID,
+			ResponseSender: func() *automock.ResponseSender {
+				sender := &automock.ResponseSender{}
+				sender.On("SendResponse", missingValueForDirectionResp, firstConn).Return(nil).Once()
+				return sender
+			},
+		},
+		{
+			Name: "fail when direction has incorrect type",
+			Room: &Room{
+				Current: &player.Player{
+					Id:   firstID,
+					Conn: firstConn,
+				},
+				Next: &player.Player{
+					Id: secondID,
+				},
+				Phase: PlaceShip,
+			},
+			Request: utils.Request{
+				PlayerId: firstID,
+				Action:   PlaceShip,
+				Args:     map[string]interface{}{"x": 2, "y": 2, "direction": player.Player{}},
+			},
+			Phase:     PlaceShip,
+			CurrentID: firstID,
+			NextID:    secondID,
+			ResponseSender: func() *automock.ResponseSender {
+				sender := &automock.ResponseSender{}
+				sender.On("SendResponse", incorrectValueTypeForDirectionResp, firstConn).Return(nil).Once()
+				return sender
+			},
+		},
+		{
+			Name: "fail when length is missing",
+			Room: &Room{
+				Current: &player.Player{
+					Id:   firstID,
+					Conn: firstConn,
+				},
+				Next: &player.Player{
+					Id: secondID,
+				},
+				Phase: PlaceShip,
+			},
+			Request: utils.Request{
+				PlayerId: firstID,
+				Action:   PlaceShip,
+				Args:     map[string]interface{}{"x": 2, "y": 2, "direction": "down"},
+			},
+			Phase:     PlaceShip,
+			CurrentID: firstID,
+			NextID:    secondID,
+			ResponseSender: func() *automock.ResponseSender {
+				sender := &automock.ResponseSender{}
+				sender.On("SendResponse", missingValueForLengthResp, firstConn).Return(nil).Once()
+				return sender
+			},
+		},
+		{
+			Name: "fail when length has incorrect type",
+			Room: &Room{
+				Current: &player.Player{
+					Id:   firstID,
+					Conn: firstConn,
+				},
+				Next: &player.Player{
+					Id: secondID,
+				},
+				Phase: PlaceShip,
+			},
+			Request: utils.Request{
+				PlayerId: firstID,
+				Action:   PlaceShip,
+				Args:     map[string]interface{}{"x": 2, "y": 2, "direction": "down", "length": "invalid"},
+			},
+			Phase:     PlaceShip,
+			CurrentID: firstID,
+			NextID:    secondID,
+			ResponseSender: func() *automock.ResponseSender {
+				sender := &automock.ResponseSender{}
+				sender.On("SendResponse", incorrectValueTypeForLengthResp, firstConn).Return(nil).Once()
+				return sender
+			},
+		},
+		{
+			Name: "success when ship placed, next player should place ship",
+			Room: &Room{
+				Current: &player.Player{
+					Id:    firstID,
+					Conn:  firstConn,
+					Board: board.InitBoard(),
+				},
+				Next: &player.Player{
+					Id:   secondID,
+					Conn: secondConn,
+				},
+				Phase:           PlaceShip,
+				ShipSizeToCount: map[int]int{destroyer: destroyerCount},
+				NextShipSize:    destroyer,
+			},
+			Request: utils.Request{
+				PlayerId: firstID,
+				Action:   PlaceShip,
+				Args:     map[string]interface{}{"x": 2, "y": 2, "direction": "down", "length": 5},
+			},
+			Phase:     PlaceShip,
+			CurrentID: secondID,
+			NextID:    firstID,
+			ResponseSender: func() *automock.ResponseSender {
+				sender := &automock.ResponseSender{}
+				sender.On("SendResponse", shipPlacedSuccessfullyResp, firstConn).Return(nil).Once()
+				sender.On("SendResponse", placeShipResp, secondConn).Return(nil).Once()
+				return sender
+			},
+		},
+		{
+			Name: "success when ship placed, next player should shoot",
+			Room: &Room{
+				Current: &player.Player{
+					Id:    firstID,
+					Conn:  firstConn,
+					Board: board.InitBoard(),
+				},
+				Next: &player.Player{
+					Id:   secondID,
+					Conn: secondConn,
+				},
+				Phase:           PlaceShip,
+				ShipSizeToCount: map[int]int{boat: 0},
+				NextShipSize:    boat,
+			},
+			Request: utils.Request{
+				PlayerId: firstID,
+				Action:   PlaceShip,
+				Args:     map[string]interface{}{"x": 2, "y": 2, "direction": "down", "length": 2},
+			},
+			Phase:     Shoot,
+			CurrentID: secondID,
+			NextID:    firstID,
+			ResponseSender: func() *automock.ResponseSender {
+				sender := &automock.ResponseSender{}
+				sender.On("SendResponse", shipPlacedSuccessfullyResp, firstConn).Return(nil).Once()
+				sender.On("SendResponse", shootResp, secondConn).Return(nil).Once()
+				return sender
+			},
+		},
+		{
+			Name: "fail when shooting and x coordinate is missing",
+			Room: &Room{
+				Current: &player.Player{
+					Id:   firstID,
+					Conn: firstConn,
+				},
+				Next: &player.Player{
+					Id: secondID,
+				},
+				Phase: Shoot,
+			},
+			Request: utils.Request{
+				PlayerId: firstID,
+				Action:   Shoot,
+			},
+			Phase:     Shoot,
+			CurrentID: firstID,
+			NextID:    secondID,
+			ResponseSender: func() *automock.ResponseSender {
+				sender := &automock.ResponseSender{}
+				sender.On("SendResponse", missingXResp, firstConn).Return(nil).Once()
+				return sender
+			},
+		},
+		{
+			Name: "fail when shooting and y coordinate has wrong type",
+			Room: &Room{
+				Current: &player.Player{
+					Id:   firstID,
+					Conn: firstConn,
+				},
+				Next: &player.Player{
+					Id: secondID,
+				},
+				Phase: Shoot,
+			},
+			Request: utils.Request{
+				PlayerId: firstID,
+				Action:   Shoot,
+				Args:     map[string]interface{}{"x": 2, "y": "y"},
+			},
+			Phase:     Shoot,
+			CurrentID: firstID,
+			NextID:    secondID,
+			ResponseSender: func() *automock.ResponseSender {
+				sender := &automock.ResponseSender{}
+				sender.On("SendResponse", incorrectValueTypeResp, firstConn).Return(nil).Once()
+				return sender
+			},
+		},
+		{
+			Name: "fail when shooting at field out of bounds",
+			Room: &Room{
+				Current: &player.Player{
+					Id:   firstID,
+					Conn: firstConn,
+				},
+				Next: &player.Player{
+					Id: secondID,
+				},
+				Phase: Shoot,
+			},
+			Request: utils.Request{
+				PlayerId: firstID,
+				Action:   Shoot,
+				Args:     map[string]interface{}{"x": 2, "y": 12},
+			},
+			Phase:     Shoot,
+			CurrentID: firstID,
+			NextID:    secondID,
+			ResponseSender: func() *automock.ResponseSender {
+				sender := &automock.ResponseSender{}
+				sender.On("SendResponse", shootOutOfBoundsResp, firstConn).Return(nil).Once()
+				return sender
+			},
+		},
+		{
+			Name: "success when shooting at field, next player should shoot",
+			Room: &Room{
+				Current: &player.Player{
+					Id:    firstID,
+					Conn:  firstConn,
+					Board: board.InitBoard(),
+				},
+				Next: &player.Player{
+					Id:    secondID,
+					Conn:  secondConn,
+					Board: getBoard(),
+				},
+				Phase: Shoot,
+			},
+			Request: utils.Request{
+				PlayerId: firstID,
+				Action:   Shoot,
+				Args:     map[string]interface{}{"x": 3, "y": 3},
+			},
+			Phase:     Shoot,
+			CurrentID: secondID,
+			NextID:    firstID,
+			ResponseSender: func() *automock.ResponseSender {
+				sender := &automock.ResponseSender{}
+				sender.On("SendResponse", shootHitResp, firstConn).Return(nil).Once()
+				sender.On("SendResponse", shootWithArgsResp, secondConn).Return(nil).Once()
+				return sender
+			},
+		},
+		{
+			Name: "enemy is defeated",
+			Room: &Room{
+				Current: &player.Player{
+					Id:    firstID,
+					Conn:  firstConn,
+					Board: board.InitBoard(),
+				},
+				Next: &player.Player{
+					Id:    secondID,
+					Conn:  secondConn,
+					Board: getBoardWithOneTakenField(3, 3),
+				},
+				Phase: Shoot,
+				Done:  make(chan struct{}, 1),
+			},
+			Request: utils.Request{
+				PlayerId: firstID,
+				Action:   Shoot,
+				Args:     map[string]interface{}{"x": 3, "y": 3},
+			},
+			Phase:     Shoot,
+			CurrentID: firstID,
+			NextID:    secondID,
+			ResponseSender: func() *automock.ResponseSender {
+				sender := &automock.ResponseSender{}
+				sender.On("SendResponse", winResp, firstConn).Return(nil).Once()
+				sender.On("SendResponse", defeatResp, secondConn).Return(nil).Once()
+				return sender
+			},
+		},
+		{
+			Name: "opponent exited",
+			Room: &Room{
+				Current: &player.Player{
+					Id:    firstID,
+					Conn:  firstConn,
+					Board: board.InitBoard(),
+				},
+				Next: &player.Player{
+					Id:    secondID,
+					Conn:  secondConn,
+				},
+				Phase: Shoot,
+				Done:  make(chan struct{}, 1),
+			},
+			Request: utils.Request{
+				PlayerId: firstID,
+				Action:   Exit,
+				Args:     nil,
+			},
+			Phase:     Shoot,
+			CurrentID: firstID,
+			NextID:    secondID,
+			ResponseSender: func() *automock.ResponseSender {
+				sender := &automock.ResponseSender{}
+				sender.On("SendResponse", exitResp, secondConn).Return(nil).Once()
+				return sender
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			// when
+			respSender := testCase.ResponseSender()
+			room := testCase.Room
+			room.ResponseSender = respSender
+			room.ProcessCommand(testCase.Request)
+
+			// then
+			assert.Equal(t, testCase.Phase, room.Phase)
+			assert.Equal(t, testCase.CurrentID, room.Current.Id)
+			assert.Equal(t, testCase.NextID, room.Next.Id)
+			respSender.AssertExpectations(t)
+		})
+	}
+}
