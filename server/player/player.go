d19ab604145417593a80f7f48331c284b2fb7c1e
Stanislav Stefanov <stanislavsvetlinovstefanov@gmail.com>
2021-02-03 00:15:52 +0200

    add player

--- server/player/player.go
+++ server/player/player.go
@@ -1,1 +1,18 @@
-package main
+package player
+
+import (
+	"github.com/StanislavStefanov/Battleships/server/board"
+	"github.com/gorilla/websocket"
+)
+
+type Player struct {
+	Conn *websocket.Conn
+	Board *board.Board
+	Id   string
+}
+
+func (p *Player) PlaceShip(ship board.Ship) error{
+	return p.Board.PlaceShip(ship)
+}
+
+
