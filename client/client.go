fc93712eb5f08cef8cd628a47d8a5a75ac74bcfa
Stanislav Stefanov <stanislavsvetlinovstefanov@gmail.com>
2021-02-08 23:48:11 +0200

    implement write loop and refactor reed loop

--- client/client.go
+++ client/client.go
@@ -5,50 +5,207 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/StanislavStefanov/Battleships/server/board"
 	"github.com/StanislavStefanov/Battleships/utils"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/url"
 	"os"
 	"os/signal"
+	"strings"
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
-func readLoop(done chan<- struct{}, conn *websocket.Conn) {
-	defer func() {
-		done <- struct{}{}
-	}()
-	for {
-		_, bytes, err := conn.ReadMessage()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		var msg utils.Response
-		json.Unmarshal(bytes, &msg)
-		fmt.Println(msg)
+const (
+	Register     = "register"
+	Exit         = "exit"
+	Shoot        = "shoot"
+	ShootOutcome = "shoot-outcome"
+	PlaceShip    = "place"
+	Placed       = "placed"
+	Wait         = "wait"
+	Retry        = "retry"
+	Win          = "win"
+	Lose         = "lose"
+	Info         = "info"
+	List         = "ls-rooms"
+	Create       = "create"
+	Created      = "created"
+	Join         = "join-room"
+	JoinRandom   = "join-random"
+)
 
-		//req := utils.BuildRequest("az", "ls-rooms", nil)
-		//marshal, _ := json.Marshal(req)
-		//conn.WriteMessage(websocket.BinaryMessage, marshal)
+type Client struct {
+	id    string
+	conn  *websocket.Conn
+	board *board.Board
+}
+
+func printMessage(resp utils.Response) {
+	fmt.Println("---------------------")
+	fmt.Println("Status: ", resp.GetAction())
+	fmt.Println("Message: ", resp.GetMessage())
+	if len(resp.GetArgs()) != 0 {
+		fmt.Println("Additional info: ", resp.GetArgs())
 	}
 }
 
-func writeLoop(done chan<- struct{}, conn *websocket.Conn) {
+func (c *Client) processResponse(resp utils.Response) {
+	printMessage(resp)
+
+	switch resp.GetAction() {
+	case Register:
+		c.id = resp.Args["id"].(string)
+	case PlaceShip:
+		c.board.Print()
+	case Placed:
+		x := int(resp.Args["x"].(float64))
+		y := int(resp.Args["y"].(float64))
+		direction := resp.Args["direction"].(string)
+		length := int(resp.Args["length"].(float64))
+
+		ship := board.CreateShip(x, y, direction, length)
+		c.board.PlaceShip(ship)
+		c.board.Print()
+	case ShootOutcome:
+		success := resp.Args["hit"].(bool)
+		x := resp.Args["x"].(int)
+		y := resp.Args["y"].(int)
+		c.board.Attack(board.Position{
+			X: x,
+			Y: y,
+		}, success)
+		c.board.Print()
+	case Shoot:
+		if len(resp.GetArgs()) != 0 {
+			x := resp.Args["x"].(int)
+			y := resp.Args["y"].(int)
+			c.board.ReceiveAttack(board.Position{
+				X: x,
+				Y: y,
+			})
+		}
+		c.board.Print()
+	}
+}
+
+func readLoop(done chan<- struct{}, client *Client) {
 	defer func() {
 		done <- struct{}{}
 	}()
 	for {
-		fmt.Println("enter command")
+		fmt.Println("read")
+		_, bytes, err := client.conn.ReadMessage()
+
+		if err != nil {
+			fmt.Println("read error", err)
+			return
+		}
+		var resp utils.Response
+		json.Unmarshal(bytes, &resp)
+		client.processResponse(resp)
+	}
+}
+
+func writeLoop(done chan<- struct{}, client *Client) {
+	defer func() {
+		done <- struct{}{}
+	}()
+	for {
+		fmt.Println("enter action")
 		buf := bufio.NewReader(os.Stdin)
-		fmt.Print("> ")
-		sentence, _ := buf.ReadBytes('\n')
-		s := string(sentence)
-		fmt.Println(s)
-		var msg = utils.BuildRequest("az", s, nil)
-		marshal, _ := json.Marshal(msg)
-		conn.WriteMessage(websocket.BinaryMessage, marshal)
+
+		b, _ := buf.ReadBytes('\n')
+		action := string(b)
+		action = strings.TrimSuffix(action, "\n")
+
+		request := utils.BuildRequest(client.id, action, nil)
+
+		switch action {
+		case Create:
+			sendRequest(request, client)
+		case List:
+			sendRequest(request, client)
+		case Join:
+			fmt.Println("enter room ID")
+			buf := bufio.NewReader(os.Stdin)
+
+			b, _ := buf.ReadBytes('\n')
+			id := string(b)
+
+			id = strings.TrimSuffix(id, "\n")
+			args := map[string]interface{}{"roomId": id}
+			request.Args = args
+
+			sendRequest(request, client)
+		case JoinRandom:
+			sendRequest(request, client)
+		case PlaceShip:
+
+			buf := bufio.NewReader(os.Stdin)
+
+			fmt.Println("enter x coordinate")
+			b, _ = buf.ReadBytes('\n')
+			x := string(b)
+			x = strings.TrimSuffix(x, "\n")
+
+			fmt.Println("enter y coordinate")
+			b, _ = buf.ReadBytes('\n')
+			y := string(b)
+			y = strings.TrimSuffix(y, "\n")
+
+			//b, _ := buf.ReadBytes('\n')
+			//x, _ := strconv.Atoi(strings.TrimSuffix(string(b), "\n"))
+			//
+			//fmt.Println("enter y coordinate")
+			//b, _ = buf.ReadBytes('\n')
+			//y, _ := strconv.Atoi(strings.TrimSuffix(string(b), "\n"))
+
+			fmt.Println("enter placement direction")
+			b, _ = buf.ReadBytes('\n')
+			direction := string(b)
+			direction = strings.TrimSuffix(direction, "\n")
+
+			args := map[string]interface{}{"x": x, "y": y, "direction": direction}
+			request.Args = args
+			sendRequest(request, client)
+		case Shoot:
+			buf := bufio.NewReader(os.Stdin)
+
+			fmt.Println("enter x coordinate")
+			b, _ = buf.ReadBytes('\n')
+			x := string(b)
+			x = strings.TrimSuffix(x, "\n")
+
+			fmt.Println("enter y coordinate")
+			b, _ = buf.ReadBytes('\n')
+			y := string(b)
+			y = strings.TrimSuffix(y, "\n")
+
+			args := map[string]interface{}{"x": x, "y": y}
+			request.Args = args
+			sendRequest(request, client)
+
+		case Exit:
+			sendRequest(request, client)
+			return
+
+		}
+
+		marshal, _ := json.Marshal(request)
+		err := client.conn.WriteMessage(websocket.BinaryMessage, marshal)
+		if err != nil {
+			fmt.Println(">>", err)
+		}
+	}
+}
+
+func sendRequest(request utils.Request, client *Client) {
+	marshal, _ := json.Marshal(request)
+	err := client.conn.WriteMessage(websocket.BinaryMessage, marshal)
+	if err != nil {
+		fmt.Println(">>", err)
 	}
 }
 
@@ -67,10 +224,14 @@ func main() {
 		log.Fatal("dial:", err)
 	}
 	defer c.Close()
-
+	client := &Client{
+		id:    "",
+		conn:  c,
+		board: board.InitBoard(),
+	}
 	done := make(chan struct{})
 
-	go readLoop(done, c)
-	go writeLoop(done, c)
+	go readLoop(done, client)
+	go writeLoop(done, client)
 	<-done
 }
