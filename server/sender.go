9ea13680b89c2127108b5c83adb6e8e3ac812ddd
Stanislav Stefanov <stanislavsvetlinovstefanov@gmail.com>
2021-02-14 23:31:05 +0200

    move sender to own file and add tests for it

--- server/sender.go
+++ server/sender.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/StanislavStefanov/Battleships/pkg/web"
+	"github.com/StanislavStefanov/Battleships/server/player"
+	"github.com/gorilla/websocket"
+)
+
+//go:generate mockery -name=ResponseSender -output=automock -outpkg=automock -case=underscore
+type ResponseSender interface {
+	SendResponse(response web.Response, conn player.Connection)
+}
+
+type Sender struct {
+}
+
+func (s *Sender) SendResponse(response web.Response, conn player.Connection) {
+	resp, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println("Send response: marshal error: ", err)
+		return
+	}
+	err = conn.WriteMessage(websocket.BinaryMessage, resp)
+	if err != nil {
+		fmt.Println("Send response: send message error: ", err)
+		return
+	}
+}
--- server/sender_test.go
+++ server/sender_test.go
@@ -1,1 +1,68 @@
-package Battleships
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/StanislavStefanov/Battleships/pkg/web"
+	connection "github.com/StanislavStefanov/Battleships/server/player/automock"
+	"github.com/gorilla/websocket"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestSender_SendResponse(t *testing.T) {
+	t.Run("fail when error occurs while writing response into connection", func(t *testing.T) {
+		// when
+		resp := web.Response{
+			Action:  "action",
+			Message: "msg",
+			Args:    nil,
+		}
+
+		rr, _ := json.Marshal(resp)
+
+		con := func() *connection.Connection {
+			con := &connection.Connection{}
+			con.On("WriteMessage", websocket.BinaryMessage, rr).Return(errors.New("error")).Once()
+			return con
+		}()
+
+		rescueStdout := os.Stdout
+		r, w, _ := os.Pipe()
+		os.Stdout = w
+
+		s := Sender{}
+		s.SendResponse(resp, con)
+
+		w.Close()
+		out, _ := ioutil.ReadAll(r)
+		os.Stdout = rescueStdout
+
+		assert.Equal(t, "Send response: send message error:  error\n", string(out))
+		con.AssertExpectations(t)
+	})
+	t.Run("success", func(t *testing.T) {
+		// when
+		resp := web.Response{
+			Action:  "action",
+			Message: "msg",
+			Args:    nil,
+		}
+
+		rr, _ := json.Marshal(resp)
+
+		con := func() *connection.Connection {
+			con := &connection.Connection{}
+			con.On("WriteMessage", websocket.BinaryMessage, rr).Return(nil).Once()
+			return con
+		}()
+
+		s := Sender{}
+		s.SendResponse(resp, con)
+
+		con.AssertExpectations(t)
+	})
+}
+
